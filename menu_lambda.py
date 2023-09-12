40184f6e78b69fee66ce52bb745dba4b663261c8
fgramajo00 <116693609+fgramajo00@users.noreply.github.com>
2023-09-11 22:35:36 -0300

    Add files via upload

---
+++ menu_lambda.py
@@ -0,0 +1,47 @@
+import os
+suma = lambda nro1, nro2: nro1+nro2
+resta = lambda nro1, nro2 : nro1 - nro2
+multiplicacion = lambda nro1, nro2 : nro1 * nro2
+division = lambda nro1,nro2: nro1 / nro2
+
+while True:
+	print("		### CALCULADORA BASICA ###")
+	print(''' 
+	1. Suma de dos numero
+	2. Resta de dis numeros
+	3. Multiplicacion de dos numeros
+	4. Division de dos numeros 
+	5. Salir ''')
+	print("")
+	opcion = input("ingrese una opcion: ")
+	if opcion == "1":	
+		os.system('clear')
+		nro1=int(input("ingrese nro: "))
+		nro2= int(input("ingrese nro: "))
+		print(f"La suma es igual a {suma(nro1,nro2)}")
+		break
+	if opcion == "2":
+		os.system('clear')		
+		nro1=int(input("ingrese nro: "))
+		nro2 = int(input("ingrese nro: "))
+		print(f"La resta es igual a {resta(nro1,nro2)}")
+		break
+	if opcion == "3":
+		os.system('clear')
+		nro1=int(input("ingrese nro: "))
+		nro2= int(input("ingrese nro: "))
+		print(f"La multiplicacion es igual a {multiplicacion(nro1,nro2)}")
+		break
+	if opcion =="4":
+		os.system('clear')
+		nro1=int(input("ingrese nro: "))
+		try: 
+			nro2 = int(input("ingrese nro: "))
+			print(f"La division es igual a {division(nro1,nro2)}")
+		except ZeroDivisionError:
+				print("La division por cero es erronea")
+		break
+	if opcion == "5":
+		break
+	else: 
+		print("opcion invalida")
